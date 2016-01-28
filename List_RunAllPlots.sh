efaa39af55745f41cdd52ea0ae84953e53b1ab20
brochero <javier.brochero.cifuentes@cern.ch>
2016-01-28 20:48:17 +0100

    To run all files

---
+++ List_RunAllPlots.sh
@@ -0,0 +1,10 @@
+root -l -b -q 'Plots.C("lepton","",true, true)'
+root -l -b -q 'Plots.C("6Jets", "",true, true)'
+root -l -b -q 'Plots.C("2btag", "",true, true)'
+root -l -b -q 'Plots.C("4btag", "",true, true)'
+
+root -l -b -q 'Plots.C("lepton","",true, false)'
+root -l -b -q 'Plots.C("6Jets", "",true, false)'
+root -l -b -q 'Plots.C("2btag", "",true, false)'
+root -l -b -q 'Plots.C("4btag", "",true, false)'
+
