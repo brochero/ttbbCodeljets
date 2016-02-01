88fd344cd30b776bd355e64732d7d320b316dc96
brochero <javier.brochero.cifuentes@cern.ch>
2016-02-02 00:19:16 +0100

    hadd to merge file for fit

---
+++ TopResults/haddFITFiles.sh
@@ -0,0 +1,9 @@
+hadd ${1}_DataSingleLep.root ${1}_DataSingleEG.root ${1}_DataSingleMu.root
+hadd ${1}_SingleTop.root ${1}_tbar_tchannel.root ${1}_tbarW.root ${1}_t_tchannel.root ${1}_tW.root
+hadd ${1}_VV.root ${1}_WW.root ${1}_WZ.root ${1}_ZZ.root
+hadd ${1}_QCD.root ${1}_QCD_MuEnr.root ${1}_QCD_EGEnr.root
+hadd ${1}_BkgFull.root ${1}_SingleTop.root ${1}_VV.root ${1}_QCD.root ${1}_ZJets_MCatNLO.root ${1}_WJets_MCatNLO.root ${1}_ttbar_PowhegPythiaBkg.root ${1}_ttbar_PowhegPythiatt.root
+hadd ${1}_ttbar_PowhegPythiattccLF.root ${1}_ttbar_PowhegPythiattcc.root ${1}_ttbar_PowhegPythiattLF.root
+hadd ${1}_ttbar_PowhegPythiaBkgtt.root ${1}_ttbar_PowhegPythiatt.root ${1}_ttbar_PowhegPythiaBkg.root
+hadd ${1}_BkgOther.root ${1}_SingleTop.root ${1}_VV.root ${1}_QCD.root ${1}_ZJets_MCatNLO.root ${1}_WJets_MCatNLO.root
+
