7d84dcb419a78c3cb786df166827c199d703137a
brochero <javier.brochero.cifuentes@cern.ch>
2016-02-02 20:32:28 +0100

    hadd syst files for the fit

---
+++ TopResults/haddFITSystFiles.sh
@@ -0,0 +1,4 @@
+hadd ${1}_BkgFull_${2}.root ${1}_SingleTop.root ${1}_VV.root ${1}_QCD.root ${1}_ZJets_MCatNLO.root ${1}_WJets_MCatNLO.root ${1}_ttbar_PowhegPythiaBkg.root ${1}_ttbar_PowhegPythia_${2}tt.root
+hadd ${1}_ttbar_PowhegPythia_${2}ttccLF.root ${1}_ttbar_PowhegPythia_${2}ttcc.root ${1}_ttbar_PowhegPythia_${2}ttLF.root
+hadd ${1}_ttbar_PowhegPythia_${2}Bkgtt.root ${1}_ttbar_PowhegPythia_${2}tt.root ${1}_ttbar_PowhegPythiaBkg.root
+
