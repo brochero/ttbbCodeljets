70e1c376bcd77938c65deaa52e419ff68af7cc70
brochero <javier.brochero.cifuentes@cern.ch>
2016-02-09 12:31:44 +0100

    After stash

--- List_RunAllPlots.sh
+++ List_RunAllPlots.sh
@@ -1,10 +1,10 @@
-root -l -b -q 'Plots.C("lepton","",true, true)'
-root -l -b -q 'Plots.C("6Jets", "",true, true)'
-root -l -b -q 'Plots.C("2btag", "",true, true)'
-root -l -b -q 'Plots.C("4btag", "",true, true)'
+root -l -b -q 'Plots.C("lepton", true)'
+root -l -b -q 'Plots.C("6Jets", true)'
+root -l -b -q 'Plots.C("2btag",true)'
+root -l -b -q 'Plots.C("4btag", true)'
 
-root -l -b -q 'Plots.C("lepton","",true, false)'
-root -l -b -q 'Plots.C("6Jets", "",true, false)'
-root -l -b -q 'Plots.C("2btag", "",true, false)'
-root -l -b -q 'Plots.C("4btag", "",true, false)'
+root -l -b -q 'Plots.C("lepton" false)'
+root -l -b -q 'Plots.C("6Jets",false)'
+root -l -b -q 'Plots.C("2btag", false)'
+root -l -b -q 'Plots.C("4btag", false)'
 
